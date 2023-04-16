9c8f51828f47aad2a8c6d25d63d8255de52db881
photovoltex <36958734+photovoltex@users.noreply.github.com>
2023-04-16 15:26:19 +0200

    add performance logging and features

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,95 @@
 use std::fs::{self, create_dir_all};
 
-use opencv::{imgcodecs, imgproc, prelude::Mat, core::{Size, Vector}};
+#[cfg(feature = "resize")]
+use opencv::{
+    core::{Size, Vector},
+    imgcodecs, imgproc,
+    prelude::Mat,
+};
 use tesseract::Tesseract;
 
 const IMG: &str = "rust_lang_org_eng1.png";
 const LANG: &str = "eng";
 
-const RESIZING: bool = false;
+#[cfg(feature = "perf_logging")]
+fn measure_millis<T>(msg: &str, f: impl FnOnce() -> T) -> T {
+    let start = std::time::Instant::now();
+    let t = f();
+    let duration = std::time::Instant::now() - start;
+    log::info!("{msg} took: {}ms", duration.as_millis());
+    t
+}
+
+#[cfg(feature = "resize")]
+fn resize(img: &str, scale: f64, read_flags: i32) -> eyre::Result<Mat> {
+    let src = imgcodecs::imread(img, read_flags)?;
+    let mut res = Mat::default();
+
+    // interpolation is chosen based on the documentation of imgproc::resize
+    let interpolation = if scale > 1. {
+        imgproc::INTER_CUBIC
+    } else {
+        imgproc::INTER_AREA
+    };
+    // take the image (src), and scale it up by given scale
+    imgproc::resize(&src, &mut res, Size::default(), scale, scale, interpolation)?;
+    Ok(res)
+}
+
+fn save(name: &str, src: Mat) -> String {
+    let out_img = format!("resized_{name}");
+    match imgcodecs::imwrite(&out_img, &src, &Vector::default()) {
+        Ok(true) => out_img,
+        failed => panic!("no success on writing file: {failed:?}"),
+    }
+}
 
 fn main() -> eyre::Result<()> {
-    let resized_img = if RESIZING {
-        let src = imgcodecs::imread(IMG, imgcodecs::IMREAD_COLOR)?;
-        let mut res = Mat::default();
-    
-        imgproc::resize(&src, &mut res, Size::default(), 3., 3., imgproc::INTER_NEAREST)?;
-    
-        let out_img = format!("resized_{IMG}");
-        match imgcodecs::imwrite(&out_img, &res,  &Vector::default()) {
-            Ok(true) => out_img,
-            failed => panic!("no success on writing file: {failed:?}")
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
+    let img = IMG.to_string();
+
+    #[cfg(feature = "resize")]
+    let img = {
+        log::info!("resizing: {img}");
+        let resize = || resize(&img, 3., imgcodecs::IMREAD_COLOR);
+
+        #[cfg(feature = "perf_logging")]
+        let res = measure_millis("resize", resize)?;
+        #[cfg(not(feature = "perf_logging"))]
+        let res = resize()?;
+
+        let save = || save(&img, res);
+        if cfg!(feature = "perf_logging") {
+            measure_millis("save", save)
+        } else {
+            save()
         }
-    } else {
-        IMG.to_string()
     };
 
-    let tess_process_img = resized_img;
-
-    let mut tess= Tesseract::new(None, Some(LANG))?
+    let tess = Tesseract::new(None, Some(LANG))?
         .set_variable("user_defined_dpi", "200")?
-        .set_image(&tess_process_img)?;
+        .set_image(&img)?;
+
+    // trigger the recognition so that we can measure it separated
+    let recognize = || tess.recognize();
+    #[cfg(feature = "perf_logging")]
+    let mut tess = measure_millis("recognize", recognize)?;
+    #[cfg(not(feature = "perf_logging"))]
+    let mut tess = recognize()?;
 
     let text = ("text", "txt", tess.get_text()?);
     let hocr = ("hocr", "html", tess.get_hocr_text(0)?);
     let tsv = ("tsv", "csv", tess.get_tsv_text(0)?);
 
-    let filename = tess_process_img.split('.').collect::<Vec<_>>().first().unwrap().to_string();
+    let filename = img
+        .split('.')
+        .collect::<Vec<_>>()
+        .first()
+        .unwrap()
+        .to_string();
     let dir = &format!("results/{LANG}/{filename}");
     create_dir_all(dir)?;
 
