f828eff2beed81e740afbdc69292cfd719d3104d
photovoltex <36958734+photovoltex@users.noreply.github.com>
2023-04-16 17:30:33 +0200

    add pdf creation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::fs::{self, create_dir_all};
+use std::fs;
 
+use opencv::prelude::MatTraitConst;
 #[cfg(feature = "resize")]
 use opencv::{
     core::{Size, Vector},
     imgcodecs, imgproc,
     prelude::Mat,
 };
+#[cfg(feature = "generate_pdf")]
+use printpdf::{Image, ImageTransform, Mm, PdfDocument};
 use tesseract::Tesseract;
 
-const IMG: &str = "rust_lang_org_eng1.png";
+const IMG: &str = "test2.png";
 const LANG: &str = "eng";
 
 #[cfg(feature = "perf_logging")]
@@ -20,9 +23,16 @@ fn measure_millis<T>(msg: &str, f: impl FnOnce() -> T) -> T {
     t
 }
 
+fn log_img(msg: &str, img: Option<&str>, x: i32, y: i32) {
+    let img = img.map(|img| format!("(img: {img})")).unwrap_or_default();
+    log::info!("{msg}{img}: x({x}):y({y})");
+}
+
 #[cfg(feature = "resize")]
 fn resize(img: &str, scale: f64, read_flags: i32) -> eyre::Result<Mat> {
     let src = imgcodecs::imread(img, read_flags)?;
+    log_img("source", Some(img), src.cols(), src.rows());
+
     let mut res = Mat::default();
 
     // interpolation is chosen based on the documentation of imgproc::resize
@@ -33,13 +43,14 @@ fn resize(img: &str, scale: f64, read_flags: i32) -> eyre::Result<Mat> {
     };
     // take the image (src), and scale it up by given scale
     imgproc::resize(&src, &mut res, Size::default(), scale, scale, interpolation)?;
+    log_img("resized", None, res.cols(), res.rows());
     Ok(res)
 }
 
+#[cfg(feature = "opencv")]
 fn save(name: &str, src: Mat) -> String {
-    let out_img = format!("resized_{name}");
-    match imgcodecs::imwrite(&out_img, &src, &Vector::default()) {
-        Ok(true) => out_img,
+    match imgcodecs::imwrite(name, &src, &Vector::default()) {
+        Ok(true) => name.to_string(),
         failed => panic!("no success on writing file: {failed:?}"),
     }
 }
@@ -51,6 +62,16 @@ fn main() -> eyre::Result<()> {
 
     let img = IMG.to_string();
 
+    // create directory to save the results
+    let split_file = img.split('.').collect::<Vec<_>>();
+
+    let (filename, ext) = (
+        split_file.first().unwrap().to_string(),
+        split_file.last().unwrap().to_string(),
+    );
+    let dir = &format!("results/{LANG}/{filename}");
+    fs::create_dir_all(dir)?;
+
     #[cfg(feature = "resize")]
     let img = {
         log::info!("resizing: {img}");
@@ -61,14 +82,18 @@ fn main() -> eyre::Result<()> {
         #[cfg(not(feature = "perf_logging"))]
         let res = resize()?;
 
-        let save = || save(&img, res);
-        if cfg!(feature = "perf_logging") {
-            measure_millis("save", save)
-        } else {
-            save()
-        }
+        let resized_img = format!("{dir}/resized.{ext}");
+        let save = || save(&resized_img, res);
+
+        #[cfg(feature = "perf_logging")]
+        let out = measure_millis("save", save);
+        #[cfg(not(feature = "perf_logging"))]
+        let out = save();
+
+        out
     };
 
+    // init tesseract
     let tess = Tesseract::new(None, Some(LANG))?
         .set_variable("user_defined_dpi", "200")?
         .set_image(&img)?;
@@ -80,24 +105,57 @@ fn main() -> eyre::Result<()> {
     #[cfg(not(feature = "perf_logging"))]
     let mut tess = recognize()?;
 
+    // retrieve recognized data from tesseract (filename, ext, data)
     let text = ("text", "txt", tess.get_text()?);
     let hocr = ("hocr", "html", tess.get_hocr_text(0)?);
     let tsv = ("tsv", "csv", tess.get_tsv_text(0)?);
 
-    let filename = img
-        .split('.')
-        .collect::<Vec<_>>()
-        .first()
-        .unwrap()
-        .to_string();
-    let dir = &format!("results/{LANG}/{filename}");
-    create_dir_all(dir)?;
-
+    // save the recognized data from tesseract
     vec![text, hocr, tsv]
         .into_iter()
-        .for_each(|(name, ext, data)| {
-            fs::write(format!("{dir}/{name}.{ext}"), data).expect("couldn't write file")
+        .for_each(|(filename, ext, data)| {
+            fs::write(format!("{dir}/{filename}.{ext}"), data).expect("couldn't write file")
         });
+
+    #[cfg(feature = "generate_pdf")]
+    let generate_pdf = || -> eyre::Result<()> {
+        let mut img_file = fs::File::open(&img)?;
+        let image = Image::try_from(image::codecs::png::PngDecoder::new(&mut img_file)?)?;
+
+        let width = image.image.width.0.try_into()?;
+        let height = image.image.height.0.try_into()?;
+        log_img("source", Some(&img), width, height);
+
+        let width = Mm(width.try_into()?);
+        let height = Mm(height.try_into()?);
+
+        let (doc, page1, layer1) = PdfDocument::new(&img, width, height, &img);
+        let layer = doc.get_page(page1).get_layer(layer1);
+
+        image.add_to_layer(
+            layer,
+            ImageTransform {
+                // todo: no magic number plz
+                dpi: Some(25.4),
+                ..Default::default()
+            },
+        );
+
+        // todo: embed the extracted ocr data into the pdf
+        doc.save(&mut std::io::BufWriter::new(
+            fs::File::create(format!("{dir}/{filename}.pdf")).unwrap(),
+        ))?;
+
+        Ok(())
+    };
+
+    #[cfg(feature = "generate_pdf")]
+    {
+        #[cfg(feature = "perf_logging")]
+        measure_millis("generate pdf", generate_pdf)?;
+        #[cfg(not(feature = "perf_logging"))]
+        generate_pdf()?;
+    }
 
     Ok(())
 }
